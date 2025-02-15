5156d9d40d8eda3d917ed8a9a39c429915e37374
haz3141 <38766403+haz3141@users.noreply.github.com>
2025-02-15 02:31:36 -0500

    Added hackathon video redirect

---
+++ app/hackathon/page.tsx
@@ -0,0 +1,23 @@
+// File: app/hackathon/page.tsx
+// Description: This file contains the page component for the hackathon 
+// page and redirects to the hackathon YouTube video pitch demo
+
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function HackathonPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to the YouTube video demo
+    router.push("https://youtu.be/nuC81oW4-lg");
+  }, [router]);
+
+  return (
+    <div className="text-center py-20">
+      <h2 className="text-2xl">Redirecting you to the Kulture.Fun Hackathon Demo...</h2>
+    </div>
+  );
+}
